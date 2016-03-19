42ffbb35f20d930dd440adcd3bdcf391dab556c0
Aishwarya <aishwaryashilpi5395@gmail.com>
2016-03-19 14:56:00 +0530

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT||3000;
@@ -19,12 +19,13 @@ app.get('/todos',function(req,res){
 
 app.get('/todos/:id',function(req,res){
 	var todoId = parseInt(req.params.id,10);
-	var matchedTodo;
-	todos.forEach(function(todo){
-		if(todo.id===todoId){
-			matchedTodo=todo;
-		}
-	});
+	var matchedTodo=_.findWhere(todos,{id: todoId});
+	// var matchedTodo;
+	// todos.forEach(function(todo){
+	// 	if(todo.id===todoId){
+	// 		matchedTodo=todo;
+	// 	}
+	// });
 	if(matchedTodo){
 		res.json(matchedTodo);
 	}else{
@@ -33,7 +34,14 @@ app.get('/todos/:id',function(req,res){
 });
 
 app.post('/todos', function(req, res){
-	var body = req.body;
+	var body = _.pick(req.body, 'description','completed');
+
+	if(!_.isBoolean(body.completed) || !_.isString(body.description)||
+		 body.description.trim().length===0){
+		return res.status(400).send();
+	}
+	body.description = body.description.trim();
+
 	//add id field
 	body.id = todoNextId++;
 	//push body into array
@@ -46,3 +54,4 @@ app.post('/todos', function(req, res){
 app.listen(PORT,function(){
 	console.log('Express listening');
 });
+
