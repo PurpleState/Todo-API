d94d965e63c24261235aa0bebdcfc7ef492ba502
Aishwarya <aishwaryashilpi5395@gmail.com>
2016-03-20 14:45:28 +0530

    Add variable playground

---
+++ variable-playground.js
@@ -0,0 +1,20 @@
+// var person={
+// 	name: 'Aishwarya',
+// 	age: 21
+// };
+
+// function updatePerson(obj){
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var grades = new Array();
+grades = [15,88];
+function addGrades(grades){
+	grades.push(55);
+	debugger;
+}
+addGrades(grades);
+console.log(grades);
